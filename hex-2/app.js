3e614c5db65e79dbb37e9aceafea95abcfd5fb11
Jim Pick <jim@jimpick.com>
2025-05-20 23:34:13 -0700

    Working end-to-end fetch

--- hex-2/app.js
+++ hex-2/app.js
@@ -8,18 +8,35 @@ iframe.addEventListener("load", onLoad);
 
 function onLoad() {
   // Listen for messages on port1
-  channel.port1.onmessage = onMessage;
+  channel.port1.onmessage = onMessageFromIFrame;
   // Transfer port2 to the iframe
+  /*
   iframe.contentWindow.postMessage(
     "test1.txt",
     "*",
     [channel.port2]
   );
+  */
 }
 
 // Handle messages received on port1
-function onMessage(e) {
+function onMessageFromIFrame(e) {
   output.innerText = e.data;
+  console.log('Frontend index.html received response from IFrame:', e.data);
+  console.log('Sending FETCH_RESPONSE message to sw');
+  /*
+  navigator.serviceWorker.controller.postMessage({
+    type: 'FETCH_RESPONSE',
+    data: e.data
+  });
+  */
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.active.postMessage({
+      type: 'FETCH_RESPONSE',
+      data: e.data
+    });
+  });
+  console.log('Sent FETCH_RESPONSE message to sw');
 }
 
 // https://github.com/mdn/dom-examples/blob/main/service-worker/simple-service-worker/app.js
@@ -43,6 +60,25 @@ const registerServiceWorker = async () => {
     } catch (error) {
       console.error(`Registration failed with ${error}`);
     }
+
+    // https://web.dev/articles/two-way-communication-guide#channel-api
+    navigator.serviceWorker.onmessage = (event) => {
+      if (event.data && event.data.type === 'FETCH') {
+        console.log('Frontend index.html received message from sw', event);
+        iframe.contentWindow.postMessage(
+          "test1.txt",
+          "*",
+          [channel.port2]
+        );
+      }
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorker/postMessage
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.active.postMessage(
+        "Test message sent immediately after creation",
+      );
+    });
   }
 };
 
@@ -60,7 +96,7 @@ async function onClick() {
   fileContents.innerHTML = "Clicked"
   const resp = await fetch("test1.txt");
   const txt = await resp.text();
-  console.log('Fetched data', txt);
+  console.log('Fetched data:', txt);
   fileContents.innerHTML = txt;
 }
 
--- hex-2/sw.js
+++ hex-2/sw.js
@@ -29,6 +29,36 @@ self.addEventListener('install', (event) => {
   */
 });
 
+let resolver = null;
+let rejecter = null;
+let origEvent = null;
+
+class TextResponse extends Response {
+  constructor(response) {
+    super();
+    this.response = response;
+  }
+  text() {
+    return new Promise.resolve(this.response);
+  }
+}
+
+self.addEventListener('message', (evt) => {
+  console.log("sw message received", evt);
+  if (evt.data && evt.data.type === 'FETCH_RESPONSE') {
+    console.log("sw fetch response:", origEvent.request.url, evt.data.data);
+    if (resolver) {
+      resolver(
+        new Response(evt.data.data, {
+          headers: {
+            'Content-Type': 'text/plain'
+          }
+        })
+      );
+    }
+  }
+});
+
 self.addEventListener('fetch', (event) => {
   console.log('sw fetch', event.request.url, event);
   /*
@@ -40,4 +70,21 @@ self.addEventListener('fetch', (event) => {
     })
   );
   */
+  // https://web.dev/articles/two-way-communication-guide#channel-api
+  if (event.request.url.endsWith("/test1.txt")) {
+    origEvent = event;
+    const promise = new Promise((resolve, reject) => {
+      // https://web.dev/articles/two-way-communication-guide#channel-api
+      resolver = resolve;
+      rejecter = reject;
+    });
+    event.respondWith(promise);
+    self.clients.matchAll().then(function (clients) {
+      if (clients && clients.length) {
+        //Respond to last focused tab
+        clients[0].postMessage({type: 'FETCH', url: event.request.url});
+      }
+    });
+  }
 });
+
